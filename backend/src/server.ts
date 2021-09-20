a9982f822b0781d9eaf7f79ab56898a8cf8c16b1
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2021-09-20 19:58:16 -0300

    finishing the 1st phase of project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,48 @@
+// Fazendo a importação do express na aplicação
 import express from 'express'
 
+// Instanciando a variável que usará e express na aplicação
 const app = express()
 
+// instanciando o uso de JSON na aplicação
+app.use(express.json())
 
+
+// Definindo um array de usuários
 const users = [
   'Eugênio',
   'Pollyana',
   'Nícolas'
 ]
 
+
+// Rota GET
 app.get('/users', (request, response) => {
   const search = String(request.query.search)
-
   const filteredUsers = search ? users.filter(user => user.includes(search)) : users
   return response.json(filteredUsers)
 })
 
+// Rota GET Com Passagem de Parâmetros
 app.get('/users/:id', (request, response) => {
   const id = Number(request.params.id)
   const user = users[id]
   return response.json(user)
 })
 
-app.post('/users', (request, response) => {
-  const user = {
-    name: "Eugênio Oliveira",
-    email: "eugenio@email.com"
-  }
 
+// Rota POST
+app.post('/users', (request, response) => {
+
+  const data = request.body
+
+  console.log(data)
+  const user = {
+    name: data.name,
+    email: data.email
+  }
   return response.json(user)
 })
 
+// Definindo a porta que o servidor utilizará para rodar a aplicação
 app.listen(3333)
