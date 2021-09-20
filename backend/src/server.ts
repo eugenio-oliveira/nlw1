9044ebbbabe9841d9c9e3adb0ea943fd597885c3
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2021-09-20 19:45:07 -0300

    changing default place for documentation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,33 @@ import express from 'express'
 
 const app = express()
 
+
+const users = [
+  'Eugênio',
+  'Pollyana',
+  'Nícolas'
+]
+
 app.get('/users', (request, response) => {
-  //console.log('listagem de usuários.')
-  return response.json({"message" : "listagem de usuários"})
+  const search = String(request.query.search)
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+  return response.json(filteredUsers)
+})
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const user = users[id]
+  return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: "Eugênio Oliveira",
+    email: "eugenio@email.com"
+  }
+
+  return response.json(user)
 })
 
 app.listen(3333)
