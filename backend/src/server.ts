178995a2b94c5376f8367b0812df50b2ceeb28dd
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2021-09-20 11:34:42 -0300

    finishing the 1st part of backend and starting frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,10 @@
 import express from 'express'
+
+const app = express()
+
+app.get('/users', (request, response) => {
+  //console.log('listagem de usuários.')
+  return response.json({"message" : "listagem de usuários"})
+})
+
+app.listen(3333)
