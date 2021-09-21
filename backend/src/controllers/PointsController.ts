ba1cc324058bc9883c565956a61dd1b76706d9f2
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2021-09-21 18:16:37 -0300

    fixing index method on pointscontroller

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -6,19 +6,20 @@ class PointsController{
 
   async index(request: Request, response: Response){
     
-    const { city, uf, items } = request.query;
+      const { city, uf, items } = request.query
 
-    const parsedItems = String(items)
-      .split(',')
-      .map(item => Number(item.trim()));
+      const parsedItems = String(items)
+        .split(',')
+        .map(item => Number(item.trim()))
+      
+      const points = await knex('points')
+        .join('point_items', 'points.id', '=', 'point_items.point_id')
+        .whereIn('point_items.item_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('points.*')
 
-    const points = await knex('points')
-      .join('point_items', 'points.id', '=', 'point_items.point_id')
-      .whereIn('point_items.item_id', parsedItems)
-      .where('city', String(city))
-      .where('uf', String(uf))
-      .distinct()
-      .select('points.*');
         return response.json(points)
   }
 
